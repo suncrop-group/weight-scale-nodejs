259231cc0f02d5eb4b99c96d1b1d62614d5d9ab0
Wajeeh Haider <75931599+Wajeeh-Haider@users.noreply.github.com>
2025-05-07 01:36:56 +0500

    Update

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
   res.send('WebSocket server is running');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
